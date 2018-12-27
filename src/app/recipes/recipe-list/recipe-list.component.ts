2401aa2211088d6747e96b1b062f0312745c317d
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2018-12-27 14:08:59 +0100

    part 2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -9,7 +9,13 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'this is a test', 'https://www.thespruceeats.com/thmb/_aVW6M204ROQkzKLBB6eu_my4pE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/goulash-2500-56a20fa53df78cf7727185be.jpg'),
+    new Recipe('Another test recipe', 'this is a test2', 'https://www.thespruceeats.com/thmb/_aVW6M204ROQkzKLBB6eu_my4pE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/goulash-2500-56a20fa53df78cf7727185be.jpg'),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 
   constructor() { }
 

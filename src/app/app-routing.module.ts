e1cab145342e6c384d9a5239c881f7489266e84b
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2019-01-18 11:40:26 +0100

    part 13 modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HomeComponent } from './core/home/home.component';
+import { AuthGuard } from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'recipes', loadChildren: './recipes/recipe.module#RecipesModule' },
   { path: 'shopping-list', component: ShoppingListComponent },
 ];
 
